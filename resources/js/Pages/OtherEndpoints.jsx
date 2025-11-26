4a67d3d91ff3e9c2b1b3cb3bf876e73b9c244805
jobelGolde12 <jobelgolde43@gmail.com>
2025-11-27 05:44:05 +0800

    Modifying vite cofig

--- resources/js/Pages/OtherEndpoints.jsx
+++ resources/js/Pages/OtherEndpoints.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
 const OtherEndpoints = () => {
-  const BASE_URL = 'http://127.0.0.1:8000/api';
+  const BASE_URL = 'http://profanityapi-production.up.railway.app:8000/api';
   
   // State for form data
   const [filipinoWord, setFilipinoWord] = useState('');
